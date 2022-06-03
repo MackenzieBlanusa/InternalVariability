5761fdd4ab5393e479fbd64dce92f85637e007a2
Mackenzie <mackenzie.blanusa@uconn.edu>
2022-06-03 17:26:48 +0000

    Completed load and merge for CMIP6.

--- LE_LoadAndMerge.py
+++ LE_LoadAndMerge.py
@@ -130,8 +130,11 @@ class LargeEnsemble():
         #drop bounds
         hist = self.drop_bounds_height(ds=hist)
         future = self.drop_bounds_height(ds=future)
-        
+        #slice to 2100, some models that go out to 2300 raise error when converting calendar 
+        future = future.sel(time=slice('2015','2100'))
         # need to add in conver_calendar 
+        hist = self.convert_calendar(ds=hist,granularity='monthly')
+        future = self.convert_calendar(ds=future,granularity='monthly')
         
         return hist, future
 
@@ -150,6 +153,65 @@ class LargeEnsemble():
         )
         return hist_path, future_path
     
+    def convert_calendar(self,ds, granularity: str):
+        """Convert to common calendar.
+
+        For "monthly", simply converts to numpy datetime objects and sets dates to first
+        of the month.
+
+        For "daily", converts all calendars to standard calendar. Leap years are ignored.
+        Error from this is assumed to be small.
+
+        Parameters
+        ----------
+        ds: xr.Dataset
+            CMIP model dataset in either daily or monthly resolution
+        granularity: str
+            Either "daily" or "monthly"
+
+        Returns
+        -------
+        ds: xr.Dataset
+            Same CMIP dataset with converted calendar
+
+        """
+
+        if granularity == 'daily':
+            if type(ds.time.values[0]) in [
+                cftime.DatetimeNoLeap,
+                cftime.DatetimeProlepticGregorian,
+                cftime.DatetimeGregorian,
+                cftime.DatetimeJulian
+            ]:
+                ds = ds.assign_coords({'time': ds.time.values.astype('datetime64[D]')})
+            elif isinstance(ds.time.values[0], cftime.Datetime360Day):
+                # Whoever came up with this should smolder in hell
+                # The code below maps the 360 days to a 365 day calendar
+                new_times = []
+                for year in np.unique(ds.time.dt.year):
+                    times360 = ds.sel(time=str(year)).time
+                    delta = np.timedelta64(int(365 / 360 * 24 * 60), 'm')
+                    new_time = np.arange(f'{year}-01-01', f'{year}-12-31',
+                                         delta, dtype='datetime64').astype('datetime64[D]')
+                    new_time = new_time[:len(times360)]
+                    new_times.append(new_time)
+                new_times = np.concatenate(new_times)
+                ds = ds.assign_coords({'time': new_times})
+
+        elif granularity == 'monthly':
+            if not type(ds.time.values[0]) is np.datetime64:
+                new_time = [
+                    np.datetime64(c.isoformat()[:10]).astype('datetime64[M]') for c in ds.time.values
+                ]
+            else:
+                new_time = ds.time.values.astype('datetime64[M]')
+            ds = ds.assign_coords({'time': new_time})
+
+        else:
+            raise NotImplementedError(f'Granularity {granularity} not implemented.')
+
+        return ds
+    
 #     def convert_lon(self,lon):
         
 #         lon = lon % 360
@@ -162,23 +224,77 @@ class LargeEnsemble():
 # cesm.hist_path
 
 
-# class MultiModelLargeEnsemble():
-#     def __init__(self, models, ... same as above):
-#         self.models = models  # e.g. ['cesm_lens', 'mpi-esm']
-#         ...
-#         self.le_dict = self.load_large_ensembles()
-#         self.hist, self.future = self.merge_datasets()
+class MultiModelLargeEnsemble():
+    def __init__(self, models, variable, granularity, lat, lon, bucket, path,
+                 load=False):
+        """Multi Model Large Ensemble class used to get CMIP6 and CESM data, and merge.
+        
+        Parameters
+        ---------
+        models: list, 
+            Name of models (source_id in intake)
+        variable: string,
+            Name of variable 
+        granularity: string,
+            "monthly" or "daily"
+        lat: float
+            Latitude, single location, from -90 to 90 
+        lon: float,
+            Longitude, single location, from -180 to 180 
+        bucket: string
+            Bucket on google cloud to save data in 
+        path: string
+            Path to save data 
+        load: True or False, default is False
+            Load saved data 
+            
+        """
+        self.models = models  # e.g. ['cesm_lens', 'mpi-esm']
+        self.variable = variable
+        self.granularity = granularity
+        self.lat = lat
+        self.lon = lon % 360
+        self.bucket = bucket
+        self.path = path 
+        self.load = load 
+        self.le_dict = self.load_large_ensembles()
+        self.hist, self.future = self.merge_datasets()
 
-#     def load_large_ensembles(self):
-#         le_dict = {}
-#         for model in self.models():
-#             le = LargeEnsemble(...)
-#             le_dict[model] = le
-#         return le_dict
+    def load_large_ensembles(self):
+        le_dict = {}
+        for model in self.models:
+            le = LargeEnsemble(model_name=model, variable=self.variable, granularity=self.granularity,
+                               lat=self.lat,lon=self.lon,bucket=self.bucket,path=self.path,load=self.load)
+            le_dict[model] = le
+        return le_dict
 
-#     def merge_dataset(self):
-#         ...
-#         return hist, future # [time, model, member]
+    def merge_datasets(self):
+        # this is ugly but it works 
+        hist_models = []
+        future_models = []
+        for model in self.models:
+            hist = self.le_dict[model].hist
+            hist = hist.drop(['lat','lon'])
+            hist = hist.assign_coords(model=model)
+            hist = hist.expand_dims('model')
+            member_number = np.arange(0,len(hist.member_id),1)
+            hist = hist.assign_coords({'member_number':member_number})
+            hist['tas'] = hist.tas.swap_dims({'member_id':'member_number'})
+            hist['member_id'] = hist.member_id.swap_dims({'member_id':'member_number'})
+            hist_models.append(hist)
+
+            future = self.le_dict[model].future
+            future = future.drop(['lat','lon'])
+            future = future.assign_coords(model=model)
+            future = future.expand_dims('model')
+            member_number = np.arange(0,len(future.member_id),1)
+            future = future.assign_coords({'member_number':member_number})
+            future['tas'] = future.tas.swap_dims({'member_id':'member_number'})
+            future['member_id'] = future.member_id.swap_dims({'member_id':'member_number'})
+            future_models.append(future)
+        future = xr.concat(future_models,dim='model')
+        hist = xr.concat(hist_models,dim='model')
+        return hist, future
 
 #     # def compute_internal_variability(self):
 
