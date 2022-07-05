601ad32784bf47ba3ef487021ded3b58995c0e25
Stephan Rasp <raspstephan@gmail.com>
2022-07-05 04:57:41 +0000

    Update

--- src/compute_extremes.py
+++ src/compute_extremes.py
@@ -3,4 +3,4 @@ from multi_model_large_ensemble import MultiModelLargeEnsemble
 mmle = MultiModelLargeEnsemble(['MIROC6', 'cesm_lens', 'CanESM5', 'MPI-ESM1-2-LR'], 'tas', 'day', 
                                lat=slice(None, None), lon=slice(None, None), 
                                bucket='climateai_data_repository', path='tmp/global_cmip_2.5deg')
-mmle.compute_x(x_type='quantile_return')
+mmle.compute_x(x_type='quantile_return', name='test1')
