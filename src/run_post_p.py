048a7dd4b76384d3709103b89146097eb478e490
carlalozu <carla@climate.ai>
2022-07-06 22:42:43 +0000

    all set!

--- src/run_post_p.py
+++ src/run_post_p.py
@@ -2,7 +2,18 @@ import xarray as xr
 from multi_model_large_ensemble import MultiModelLargeEnsemble
 from post_p_large_ensemble import qdm_large_ensemble
 
-from app.main.src.utils import cmip2era
+era2cmip = {
+    't2m': 'tas',
+    't2m_max': 'tasmax',
+    't2m_min': 'tasmin',
+    'tp': 'pr',
+    'huss': 'huss',  # Specific humidity does not exist in ERA, so just mapping CMIP to CMIP
+    # 'd2m': 'tdps',
+    'ws': 'sfcWind',
+    'sm': 'mrsos'  # https://agupubs.onlinelibrary.wiley.com/doi/epdf/10.1029/2020GL089991
+}
+
+cmip2era = {v: k for k, v in era2cmip.items()}
 
 regions_dict = {
     'USeast': {'lat': 41.3, 'lon': -72.5},
@@ -21,7 +32,7 @@ models_for_vars = {
     'tasmax': ['CanESM5', 'cesm_lens', 'MIROC6', 'MPI-ESM1-2-LR', 'EC-Earth3'],
 }
 # ,['USwest', 'europe', 'australia', 'tropics', 'USeast', 'iceland']:
-for region in ['australia']:
+for region in ['USwest', 'europe', 'tropics', 'USeast', 'iceland']:
     lat = regions_dict[region]['lat']
     lon = regions_dict[region]['lon']
     print(f'Processing {region} future for {variable}')
@@ -35,23 +46,31 @@ for region in ['australia']:
     path = f'gcs://climateai_data_repository/tmp/internal_variability/era_files/{region}/reanalysis_daily.zarr'
     reanalysis_daily = xr.open_zarr(path, consolidated=True).load()
 
+    if variable in ('tasmax', 'tas'):
+        reanalysis_daily[cmip2era[variable]] += 273.15
+
+    print('Processing historical models')
     large_ens_hist = qdm_large_ensemble(
         MMLE.hist_dsets,
         MMLE.hist_dsets,
         reanalysis_daily[cmip2era[variable]]
     )
 
+    print('Processing future models')
     large_ens_future = qdm_large_ensemble(
         MMLE.future_dsets,
         MMLE.hist_dsets,
         reanalysis_daily[cmip2era[variable]]
     )
 
-    for model in large_ens_future.keys():
-        future_path = f'gcs://{MMLE.bucket}/tmp/qdm_{region}/{model}/historical/{MMLE.granularity}/{MMLE.variable}.zarr'
-        large_ens_future[model].to_dataset(name=variable).to_zarr(path, consolidated=True, mode='w')
-
-        hist_path = f'gcs://{MMLE.bucket}/tmp/qdm_{region}/{model}/{MMLE.scenario}/{MMLE.granularity}/{MMLE.variable}.zarr'
-        large_ens_hist[model].to_dataset(name=variable).to_zarr(path, consolidated=True, mode='w')
-
     print(f'Done post-processing {region}!')
+    print(f'Uploading {region} files to GCS')
+
+    for model in large_ens_future.keys():
+        future_path = f'gcs://{MMLE.bucket}/tmp/qdm_{region}/{model}/{MMLE.scenario}/{MMLE.granularity}/{MMLE.variable}.zarr'
+        large_ens_future[model].to_dataset(name=variable).to_zarr(future_path, consolidated=True, mode='w')
+
+        hist_path = f'gcs://{MMLE.bucket}/tmp/qdm_{region}/{model}/historical/{MMLE.granularity}/{MMLE.variable}.zarr'
+        large_ens_hist[model].to_dataset(name=variable).to_zarr(hist_path, consolidated=True, mode='w')
+
+    print(f'Done uploading {region}!')
