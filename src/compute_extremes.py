b49fa03d7f53d2aa0af0847a92d131648b96e856
Stephan Rasp <raspstephan@gmail.com>
2022-07-14 07:08:25 +0000

    Update to precupute scripts

--- src/compute_extremes.py
+++ src/compute_extremes.py
@@ -1,18 +1,28 @@
 from multi_model_large_ensemble import MultiModelLargeEnsemble
 
 
-mmle = MultiModelLargeEnsemble(['MIROC6', 'cesm_lens', 'CanESM5', 'MPI-ESM1-2-LR', 'EC-Earth3'], 'tasmax', 'day', 
-                               lat=slice(None, None), lon=slice(None, None), 
-                               bucket='climateai_data_repository', path='tmp/global_cmip_2.5deg')
-mmle.compute_x(x_type='quantile_return', name='tasmax_default_quantile_return_10yr')
+# mmle = MultiModelLargeEnsemble(['MIROC6', 'cesm_lens', 'CanESM5', 'MPI-ESM1-2-LR', 'EC-Earth3'], 'tasmax', 'day', 
+#                                lat=slice(None, None), lon=slice(None, None), 
+#                                bucket='climateai_data_repository', path='tmp/global_cmip_2.5deg')
+# mmle.compute_x(x_type='quantile_return', name='tasmax_default_quantile_return_10yr')
 
-mmle = MultiModelLargeEnsemble(['MIROC6', 'cesm_lens', 'CanESM5', 'MPI-ESM1-2-LR', 'EC-Earth3'], 'pr', 'day', 
-                               lat=slice(None, None), lon=slice(None, None), 
-                               bucket='climateai_data_repository', path='tmp/global_cmip_2.5deg')
-mmle.compute_x(x_type='quantile_return', name='pr_default_quantile_return_10yr')
+# mmle = MultiModelLargeEnsemble(['MIROC6', 'cesm_lens', 'CanESM5', 'MPI-ESM1-2-LR', 'EC-Earth3'], 'pr', 'day', 
+#                                lat=slice(None, None), lon=slice(None, None), 
+#                                bucket='climateai_data_repository', path='tmp/global_cmip_2.5deg')
+# mmle.compute_x(x_type='quantile_return', name='pr_default_quantile_return_10yr')
 
-mmle = MultiModelLargeEnsemble(['MIROC6', 'cesm_lens', 'CanESM5', 'MPI-ESM1-2-LR', 'EC-Earth3'], 'tas', 'day', 
-                               lat=slice(None, None), lon=slice(None, None), 
-                               bucket='climateai_data_repository', path='tmp/global_cmip_2.5deg')
-mmle.compute_x(x_type='quantile_return', name='tas_default_quantile_return_10yr')
+# mmle = MultiModelLargeEnsemble(['MIROC6', 'cesm_lens', 'CanESM5', 'MPI-ESM1-2-LR', 'EC-Earth3'], 'tas', 'day', 
+#                                lat=slice(None, None), lon=slice(None, None), 
+#                                bucket='climateai_data_repository', path='tmp/global_cmip_2.5deg')
+# mmle.compute_x(x_type='quantile_return', name='tas_default_quantile_return_10yr')
+
+
+scenarios = ['ssp126', 'ssp245', 'ssp370', 'ssp585']
+variables = ['tasmax', 'tas', 'pr']
+for v in variables:
+    for s in scenarios:
+        mmle = MultiModelLargeEnsemble('cmip6', v, 'day', 
+                                       lat=slice(None, None), lon=slice(None, None), scenario=s,
+                                       bucket='climateai_data_repository', path='tmp/global_cmip_2.5deg')
+        mmle.compute_x(x_type='quantile_return', name=f'cmip6_{s}_{v}_default_quantile_return_10yr')
 
